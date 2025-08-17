24c7656b4e826404dd37678a9352c1f2bcfe4f35
amir <amirtaki623@gmail.com>
2025-08-17 23:35:07 +0330

    part 8

--- src/main.js
+++ src/main.js
@@ -1,3 +1,8 @@
 let x = 0;
 const output = document.getElementById("output");
 const meter = document.getElementById("meter");
+
+output.textContent = x;
+const plus = () => {
+    output.innerHTML = ++x;
+}
