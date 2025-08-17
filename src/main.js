1536f3dff0580388e37c27246e93073238ecc6d3
amir <amirtaki623@gmail.com>
2025-08-17 23:38:08 +0330

    part 10

--- src/main.js
+++ src/main.js
@@ -4,9 +4,15 @@ const meter = document.getElementById("meter");
 
 output.textContent = x;
 const plus = () => {
+    if( x >= 10){
+        return false
+    }
     output.textContent = ++x;
 }
 
 const minus = () => {
+    if (x <= 0) {
+        return false
+    }
     output.textContent = --x;
 }
